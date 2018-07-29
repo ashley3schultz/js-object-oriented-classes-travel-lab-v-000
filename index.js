7d70a08e0fab0a8c5e7fbe2076da71015ff8e52c
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-29 19:16:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ class Route {
   blocksTravelled(){
     const vs = this.beginningLocation.vertical
     const ve = this.endingLocation.vertical
-    const hs = this.beginningLocation.horizontal === 'Park' ? 4 : 1
-    const he = this.endingLocation.horizontal === 'Park' ? 4 : 1
+    const hs = this.beginningLocation.horizontal === 'Park' ? 5 : 1
+    const he = this.endingLocation.horizontal === 'Park' ? 5 : 1
     const v = ve > vs ? ve - vs : vs - ve
     const h = he > hs ? he - hs : hs - he
     return v > h ? v - h : h - v
