e1f0d8feb9da1e91235722b2e38bf111e1a670a9
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-29 19:18:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ class Route {
     const ve = this.endingLocation.vertical
     const hs = this.beginningLocation.horizontal === 'Park' ? 5 : 1
     const he = this.endingLocation.horizontal === 'Park' ? 5 : 1
-    const v = ve > vs ? ve - vs : vs - ve
-    const h = he > hs ? he - hs : hs - he
-    return v + h
+    return (ve > vs ? ve - vs : vs - ve) - he > hs ? he - hs : hs - he
   }
 
   estimatedTime(){
